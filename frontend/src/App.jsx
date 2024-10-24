c9ab922b7908246e9a0c0eb1ef0bfc6dc2bd62b7
KC1064 <kiron08072003@gmail.com>
2024-10-24 19:18:11 +0530

    Error in Navbar link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,6 +27,7 @@ const App = () => {
       element: <Signup />,
     },
   ]);
+  
   return (
     <div
       style={{
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,18 @@
 import { Avatar } from "@mui/material";
 import { yellow } from "@mui/material/colors";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="w-full h-max px-5 py-4 lg:py-6 flex items-center justify-between">
-      <img
-        src="./logo.png"
-        alt="Logo"
-        className="h-10 w-10 lg:h-16 lg:w-16 rounded-full border-2 border-yellow-400"
-      />
+      <Link to={"/"}>
+        <img
+          src="./logo.png"
+          alt="Logo"
+          className="h-10 w-10 lg:h-16 lg:w-16 rounded-full border-2 border-yellow-400"
+        />
+      </Link>
       <Avatar
         sx={{
           border: yellow[400],
